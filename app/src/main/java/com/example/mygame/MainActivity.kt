33f7e1caf676ea4381f489be10233a39215cf0b0
raveen thrimawithana <raveenthrimawithana111@gmail.com>
2023-11-24 02:09:01 +0200

    [UI] Adding Composable UI
    Adding composable dependencies
    Adding layout resources folder
    Adding main layout
    Inherit onCreate

--- app/src/main/java/com/example/mygame/MainActivity.kt
+++ app/src/main/java/com/example/mygame/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.mygame
 
+import android.os.Bundle
+
+import android.util.Log
 import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.core.view.ViewCompat
 import com.google.androidgamesdk.GameActivity
 
 class MainActivity : GameActivity() {
@@ -10,6 +20,35 @@ class MainActivity : GameActivity() {
         }
     }
 
+    override fun onCreateSurfaceView() {
+        this.setContentView(R.layout.main_activity)
+        Log.e("raveen", "calling self")
+        mSurfaceView = InputEnabledSurfaceView(this)
+        val frameLayout = findViewById<FrameLayout>(R.id.frame_layout)
+        frameLayout.addView(mSurfaceView)
+        val mTextView = TextView(this)
+        mTextView.text = "hello"
+        frameLayout.addView(mTextView)
+        frameLayout.requestFocus()
+        mSurfaceView.holder.addCallback(this)
+        ViewCompat.setOnApplyWindowInsetsListener(mSurfaceView, this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)  {
+        Log.e("raveen", "calling onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Surface() {
+
+                    Text(
+                        text = name,
+                        modifier = modifier
+                    )
+        }
+    }
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
